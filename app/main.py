609ccb77a8daa941a205a2a8e453cc1d7ac7de90
Eliot <eliot.amanieu@epitech.eu>
2025-01-12 15:00:21 +0100

    feat(backend): Env variables importing

--- app/main.py
+++ app/main.py
@@ -1,23 +1,36 @@
 import json
+import os
+from asyncio import timeout
+
 import pymongo
 from flask import Flask
 
 from app.api.routes.scrapers_routes import load_scrapers_routes
 from app.globals import Globals
-from app.tools.teklogger import log_info, log_debug, log_error
-
-
+from app.tools.envloader import load_env
+from app.tools.teklogger import log_info, log_debug, log_error, log_success
 
 if __name__ == '__main__':
     log_info("Welcome to TekBetter server !")
     log_debug("Debug mode enabled")
     try:
-        pass  # load_env()
+        load_env()
     except Exception as e:
         log_error(str(e))
         exit(1)
-    Globals.database = pymongo.MongoClient("mongodb://localhost:27017")["tekbetter"]
-    log_info("Connected to MongoDB")
+
+    mongo_url = f"mongodb://{os.getenv('MONGO_HOST')}:{os.getenv('MONGO_PORT')}"
+
+    log_info("Connecting to MongoDB...")
+    Globals.database = pymongo.MongoClient(mongo_url)[os.getenv('MONGO_DB')]
+
+    # Check if the connection is successful
+    try:
+        Globals.database.list_collection_names()
+    except:
+        log_error("Failed to connect to MongoDB server at " + mongo_url)
+        exit(1)
+    log_success("Connected to MongoDB")
 
     Globals.app = Flask(__name__)
     load_scrapers_routes()
---
+++ app/tools/envloader.py
@@ -0,0 +1,33 @@
+import os
+
+from dotenv import load_dotenv
+
+from app.tools.teklogger import log_debug, log_warning
+
+default_values = {
+    "PORT": "3000",
+    "MONGO_HOST": "mongo",
+    "MONGO_PORT": "27017",
+    "MONGO_DB": "tekbetter",
+    "JWT_SECRET": None,
+    "AES_KEY": None,
+}
+
+
+def load_env():
+    """
+    Load environment variables from the .env file
+    Raises an exception if a required environment variable is missing
+    """
+
+    log_debug("Loading environment variables")
+    load_dotenv()
+    for key, value in default_values.items():
+        if os.getenv(key, None) is None:
+            if value is not None:
+                log_warning(f"Missing environment variable {key}, using default value: {value}")
+                os.environ[key] = value
+            else:
+                raise Exception(f"Missing environment variable {key}")
+    if len(os.getenv("AES_KEY")) != 64:
+        raise Exception("AES_KEY must be 64 characters long")
