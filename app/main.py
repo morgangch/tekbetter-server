289f8f43a84a6a3ddd8b564e15b3117b865dc2ea
Eliot <eliot.amanieu@epitech.eu>
2025-01-16 14:43:09 +0100

    fix(backend): startup functions

--- app/main.py
+++ app/main.py
@@ -82,13 +82,13 @@ def create_app():
     load_auth_routes(flask_app)
 
     # Serve React App
-    @app.route('/', defaults={'path': ''})
-    @app.route('/<path:path>')
+    @flask_app.route('/', defaults={'path': ''})
+    @flask_app.route('/<path:path>')
     def serve(path):
-        if path != "" and os.path.exists(app.static_folder + '/' + path):
-            return send_from_directory(app.static_folder, path)
+        if path != "" and os.path.exists(flask_app.static_folder + '/' + path):
+            return send_from_directory(flask_app.static_folder, path)
         else:
-            return send_from_directory(app.static_folder, 'index.html')
+            return send_from_directory(flask_app.static_folder, 'index.html')
 
 
     CORS(flask_app)
