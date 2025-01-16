4ee2872d011ebe479347ce9f337be9fc02ec8421
Eliot <eliot.amanieu@epitech.eu>
2025-01-16 17:15:37 +0100

    feat(dashboard): auto-redirecting root route to /sync

--- web/src/pages/HomePage.tsx
+++ web/src/pages/HomePage.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
 import TopBar from "../comps/TopBar";
+import {useNavigate} from "react-router";
 
 export default function HomePage(): React.ReactElement {
+    const nav = useNavigate();
+
+    useEffect(() => {
+        nav("/sync");
+    }, []);
     return (
         <TopBar/>
     );
